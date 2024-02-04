115943fc01ff07098bf71e0df59084da8655dc40
Dave Chroninger <52579889+dchroninger@users.noreply.github.com>
2024-02-04 11:09:46 -0800

    refactor for readability

--- main.go
+++ main.go
@@ -12,9 +12,7 @@ import (
 
 type key int
 
-const (
-	keyServerAddr key = iota
-)
+const keyServerAddr key = iota
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
@@ -31,56 +29,41 @@ func getAdminRoot(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", getRoot)
-
-	adminMux := http.NewServeMux()
-	adminMux.HandleFunc("/", getAdminRoot)
-
+	// Create a context that can be cancelled
 	ctx, cancel := context.WithCancel(context.Background())
 
+	// Public server
+	mux := http.NewServeMux()
 	port := getEnvWithDefault("PORT", "3333")
+
+	// Public server routes
+	mux.HandleFunc("/", getRoot)
+
+	// Admin server
+	adminMux := http.NewServeMux()
 	adminPort := getEnvWithDefault("ADMIN_PORT", "4444")
 
-	publicServer := &http.Server{
-		Addr:    ":" + port,
-		Handler: mux,
-		BaseContext: func(l net.Listener) context.Context {
-			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
-			return ctx
-		},
-	}
+	// Admin server routes
+	adminMux.HandleFunc("/", getAdminRoot)
 
-	go func() {
-		err := publicServer.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Println("public server closed")
-		} else {
-			fmt.Println("public server error:", err)
-		}
-		cancel()
-	}()
+	publicServer := buildHttpServer(port, mux, keyServerAddr, ctx)
+	adminServer := buildHttpServer(adminPort, adminMux, keyServerAddr, ctx)
 
-	adminServer := &http.Server{
-		Addr:    ":" + adminPort,
-		Handler: adminMux,
-		BaseContext: func(l net.Listener) context.Context {
-			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
-			return ctx
-		},
-	}
-
-	go func() {
-		err := adminServer.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Println("admin server closed")
-		} else {
-			fmt.Println("admin server error:", err)
-		}
-		cancel()
-	}()
+	// Launch the servers in goroutines
+	go listenAndServe("Public Server", publicServer, cancel)
+	go listenAndServe("Admin Server", adminServer, cancel)
 
 	<-ctx.Done()
+}
+
+func listenAndServe(name string, s *http.Server, c context.CancelFunc) {
+	err := s.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("%s closed\n", name)
+	} else {
+		fmt.Printf("%s error: %s", name, err)
+	}
+	c()
 }
 
 func getEnvWithDefault(key, defaultValue string) string {
@@ -89,4 +72,14 @@ func getEnvWithDefault(key, defaultValue string) string {
 		return defaultValue
 	}
 	return value
+}
+
+func buildHttpServer(port string, mux *http.ServeMux, ctxKey key, ctx context.Context) *http.Server {
+	return &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			return context.WithValue(ctx, ctxKey, l.Addr().String())
+		},
+	}
 }
