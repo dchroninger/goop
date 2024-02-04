ba652a54c018feb0a33b9aa70347dbd7f89c345d
Dave Chroninger <52579889+dchroninger@users.noreply.github.com>
2024-02-04 13:50:30 -0800

    added route to serve css

--- routes.go
+++ routes.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+func css(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "css/output.css")
+}
+
 func index(w http.ResponseWriter, _ *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
 	tmpl.Execute(w, nil)
